81da4a842fb41673b809b095b45577e162fa3037
jeonghoheo <heojeongho1992@gmail.com>
2020-01-03 14:16:31 +0900

    add - mutation을 사용해 유저 추가해보기

--- src/Users.js
+++ src/Users.js
@@ -1,24 +1,59 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 import { ROOT_QUERY } from "./App";
 
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
 const Users = () => {
-  const { data, loading } = useQuery(ROOT_QUERY);
+  const { data, loading, refetch } = useQuery(ROOT_QUERY);
   return (
     <>
       {loading ? (
         <p>사용자를 불러오는중...</p>
       ) : (
-        <UserLIst count={data.totalUsers} users={data.allUsers} />
+        <UserLIst
+          count={data.totalUsers}
+          users={data.allUsers}
+          refetchUsers={refetch}
+        />
       )}
     </>
   );
 };
 
-const UserLIst = ({ count, users }) => {
+const UserLIst = ({ count, users, refetchUsers }) => {
+  const [addFakeUsers] = useMutation(ADD_FAKE_USERS_MUTATION);
+  const variables = {
+    count: 1
+  };
   return (
     <div>
       <p>{count} Users</p>
+      <button
+        onClick={() => {
+          refetchUsers();
+        }}
+      >
+        다시 가져오기
+      </button>
+      <button
+        onClick={() =>
+          addFakeUsers({
+            variables
+          })
+        }
+      >
+        임시 사용자 추가
+      </button>
       <ul>
         {users.map(user => (
           <UserLIstItem key={user.name} name={user.name} avatar={user.avatar} />
